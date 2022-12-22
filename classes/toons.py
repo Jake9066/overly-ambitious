f649aa64a383dde5d68841638eb749d8c851005e
Jake9066 <hamilt.ch@gmail.com>
2022-12-22 15:17:31 -0600

    Update toons.py

--- classes/toons.py
+++ classes/toons.py
@@ -1,5 +1,15 @@
+from .skills import *
+
 class Character:
-  pass
+  def __init__(self):
+    self.Strength = 0
+    self.Dexterity = 0
+    self.Constitution = 0
+    self.Intelligence = 0
+    self.Wisdom = 0
+    self.Charisma = 0
+    
+    self.Skills = Skills()
 
 class PlayerCharacter(Character):
   pass
